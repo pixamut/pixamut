89dbb15297e9958631a54da5e0fc4507f667b0fc
Raouf <raouf.hec@gmail.com>
2025-04-06 15:21:32 +0200

    free for all

--- backend/src/contracts/project_listeners.py
+++ backend/src/contracts/project_listeners.py
@@ -108,111 +108,21 @@ async def project_execution_loop():
             # 1 - get all projects
             projects = await PROJECTS.get_many(db, limit=10)
             for project in projects:
-                print("gas used", wei_to_ether(project.gas_used), flush=True)
-                if (
-                    project.balance > 0
-                    and (project.gas_available - project.gas_used) > 100_000_000
-                ):
-                    dbProjectSnapshot = await PROJECT_SNAPSHOTS.get(
-                        db, project_address=project.address
-                    )
-                    if dbProjectSnapshot is None:
-                        nbr_controlled_pixels = await PIXELS.get_owner_pixel_count(
-                            db, owner=project.address
+                try:
+                    print("gas used", wei_to_ether(project.gas_used), flush=True)
+                    # if (
+                    #     # project.balance > 0
+                    #     # and (project.gas_available - project.gas_used) > 100_000_000
+                    # ):
+                    if True:
+                        dbProjectSnapshot = await PROJECT_SNAPSHOTS.get(
+                            db, project_address=project.address
                         )
-                        dbProjectSnapshot = await PROJECT_SNAPSHOTS.create(
-                            db,
-                            obj_in=ProjectSnapshotCreate(
-                                address=project.address,
-                                title=project.title,
-                                nbr_active_pixels=project.nbr_active_pixels,
-                                nbr_controlled_pixels=nbr_controlled_pixels,
-                                balance=project.balance,
-                                gas_used=project.gas_used,
-                                gas_available=project.gas_available,
-                                best_row=project.best_row,
-                                best_col=project.best_col,
-                                best_cost=project.best_cost,
-                            ),
-                        )
-                    balance_in_eth = wei_to_ether(project.balance)
-                    real_balance = await token_contract.functions.balanceOf(
-                        provider.to_checksum_address(project.address)
-                    ).call()  # type: ignore
-                    real_balance_in_eth = wei_to_ether(real_balance)
-                    budget_in_eth = real_balance_in_eth
-
-                    if budget_in_eth <= 1:
-                        print("not enough budget", budget_in_eth, flush=True)
-                        continue
-                    # 2 - find the best placement
-                    image_grid, image_mask = image_to_np(project.image)
-                    # image_grid = np.frombuffer(
-                    #     project.image_grid, dtype=np.uint32
-                    # ).reshape(project.image_h, project.image_w)
-                    # image_mask = np.frombuffer(
-                    #     project.image_mask, dtype=np.bool_
-                    # ).reshape(project.image_h, project.image_w)
-                    # image_mask = image_mask.astype(bool)
-
-                    print("Mask shape", image_mask.shape, flush=True)
-                    best_row, best_col, best_cost = find_best_placement_full(
-                        image_color=image_grid,
-                        image_mask=image_mask,
-                        new_owner=hash_address(project.address),
-                        budget=budget_in_eth,
-                        eager=False,
-                    )
-                    if (
-                        best_row is not None
-                        and best_col is not None
-                        and best_cost is not None
-                    ):
-                        project.best_row = best_row
-                        project.best_col = best_col
-                        project.best_cost = int(best_cost * 1e18)
-                        remaining_budget = budget_in_eth - best_cost
-                        if remaining_budget > 1:
-                            remaining_budget -= 1.0
-                        if remaining_budget <= 1:
-                            remaining_budget = 0.0
-                        print(
-                            "BEST ROW, COL =============",
-                            best_row,
-                            best_col,
-                            "remaining budget",
-                            remaining_budget,
-                            flush=True,
-                        )
-                        db.add(project)
-                        await db.commit()
-                        actions = generate_actions_for_placement(
-                            image_grid=image_grid,
-                            image_mask=image_mask,
-                            nbr_active_pixels=project.nbr_active_pixels,
-                            base_row=best_row,
-                            base_col=best_col,
-                            project_address=project.address,
-                            unused_budget=remaining_budget,
-                        )
-                        # dbActions = await ACTIONS.setActions(
-                        #     db, project_address=project.address, actions=actions
-                        # )
-
-                        for dbAction in actions:
-                            await run_action(db, action=dbAction, project=project)
-                            # wait between tx
-                            # await asyncio.sleep(2)
-
-                        await asyncio.sleep(1)
-                        if len(actions) > 0:
-                            print(
-                                "gas used", wei_to_ether(project.gas_used), flush=True
-                            )
+                        if dbProjectSnapshot is None:
                             nbr_controlled_pixels = await PIXELS.get_owner_pixel_count(
                                 db, owner=project.address
                             )
-                            await PROJECT_SNAPSHOTS.create(
+                            dbProjectSnapshot = await PROJECT_SNAPSHOTS.create(
                                 db,
                                 obj_in=ProjectSnapshotCreate(
                                     address=project.address,
@@ -227,16 +137,114 @@ async def project_execution_loop():
                                     best_cost=project.best_cost,
                                 ),
                             )
+                        balance_in_eth = wei_to_ether(project.balance)
+                        real_balance = await token_contract.functions.balanceOf(
+                            provider.to_checksum_address(project.address)
+                        ).call()  # type: ignore
+                        real_balance_in_eth = wei_to_ether(real_balance)
+                        budget_in_eth = real_balance_in_eth
 
-                else:
-                    print(
-                        "Not enough gas or balance",
-                        project.balance,
-                        project.gas_available,
-                        project.gas_used,
-                        flush=True,
-                    )
-                # wait between projects
-                await asyncio.sleep(1)
+                        if budget_in_eth <= 1:
+                            print("not enough budget", budget_in_eth, flush=True)
+                            continue
+                        # 2 - find the best placement
+                        image_grid, image_mask = image_to_np(project.image)
+                        # image_grid = np.frombuffer(
+                        #     project.image_grid, dtype=np.uint32
+                        # ).reshape(project.image_h, project.image_w)
+                        # image_mask = np.frombuffer(
+                        #     project.image_mask, dtype=np.bool_
+                        # ).reshape(project.image_h, project.image_w)
+                        # image_mask = image_mask.astype(bool)
+
+                        print("Mask shape", image_mask.shape, flush=True)
+                        best_row, best_col, best_cost = find_best_placement_full(
+                            image_color=image_grid,
+                            image_mask=image_mask,
+                            new_owner=hash_address(project.address),
+                            budget=budget_in_eth,
+                            eager=False,
+                        )
+                        if (
+                            best_row is not None
+                            and best_col is not None
+                            and best_cost is not None
+                        ):
+                            project.best_row = best_row
+                            project.best_col = best_col
+                            project.best_cost = int(best_cost * 1e18)
+                            remaining_budget = budget_in_eth - best_cost
+                            if remaining_budget > 1:
+                                remaining_budget -= 1.0
+                            if remaining_budget <= 1:
+                                remaining_budget = 0.0
+                            print(
+                                "BEST ROW, COL =============",
+                                best_row,
+                                best_col,
+                                "remaining budget",
+                                remaining_budget,
+                                flush=True,
+                            )
+                            db.add(project)
+                            await db.commit()
+                            actions = generate_actions_for_placement(
+                                image_grid=image_grid,
+                                image_mask=image_mask,
+                                nbr_active_pixels=project.nbr_active_pixels,
+                                base_row=best_row,
+                                base_col=best_col,
+                                project_address=project.address,
+                                unused_budget=remaining_budget,
+                            )
+                            # dbActions = await ACTIONS.setActions(
+                            #     db, project_address=project.address, actions=actions
+                            # )
+
+                            for dbAction in actions:
+                                await run_action(db, action=dbAction, project=project)
+                                # wait between tx
+                                # await asyncio.sleep(2)
+
+                            await asyncio.sleep(1)
+                            if len(actions) > 0:
+                                print(
+                                    "gas used",
+                                    wei_to_ether(project.gas_used),
+                                    flush=True,
+                                )
+                                nbr_controlled_pixels = (
+                                    await PIXELS.get_owner_pixel_count(
+                                        db, owner=project.address
+                                    )
+                                )
+                                await PROJECT_SNAPSHOTS.create(
+                                    db,
+                                    obj_in=ProjectSnapshotCreate(
+                                        address=project.address,
+                                        title=project.title,
+                                        nbr_active_pixels=project.nbr_active_pixels,
+                                        nbr_controlled_pixels=nbr_controlled_pixels,
+                                        balance=project.balance,
+                                        gas_used=project.gas_used,
+                                        gas_available=project.gas_available,
+                                        best_row=project.best_row,
+                                        best_col=project.best_col,
+                                        best_cost=project.best_cost,
+                                    ),
+                                )
+
+                    else:
+                        print(
+                            "Not enough gas or balance",
+                            project.balance,
+                            project.gas_available,
+                            project.gas_used,
+                            flush=True,
+                        )
+                    # wait between projects
+                    await asyncio.sleep(1)
+                except Exception as e:
+                    print("Error executing project:", str(e), flush=True)
         # wait between complete projects sweep
         await asyncio.sleep(5)
